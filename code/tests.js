ecaa05171b03240284bc0e67f80a498d84869fa6
Evan Carmi <evan@ecarmi.org>
2012-11-04 16:12:54 -0500

    Moving some tests to use QUnit api methods

--- code/tests.js
+++ code/tests.js
@@ -3,28 +3,20 @@
  * http://qunitjs.com
  */
 
-test( "Example test", function() {
-  ok( 1 == "1", "Passed!" );
-});
-
 test("gcd", function() {
   ok( gcd(54, 24) === 6, "gcd(54, 24) returns 6");
 });
 
-// For array equality
-function arrays_equal(a,b) { return !(a<b || b<a); }
-
-// still normal ints
 test("egcd", function() {
-  ok( arrays_equal(egcd(54,24), [6,1,-2]), "extended gcd works correctly");
+  deepEqual(egcd(54,24), [6,1,-2], "extended gcd works correctly");
 });
 
 test("egcd", function() {
-  ok( arrays_equal(egcd(54,21), [3,2,-5]), "extended gcd works correctly");
+  deepEqual(egcd(54,21), [3,2,-5], "extended gcd works correctly");
 });
 
 test("mod_inverse", function() {
-  ok( mod_inverse(8, 19) === 12, "mod_inverse works");
+ strictEqual(mod_inverse(8, 19), 12, "mod_inverse works");
 });
 
 // Now uses big ints
